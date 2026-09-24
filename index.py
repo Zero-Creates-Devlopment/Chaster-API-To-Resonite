51ca0427a639e563fea5c4de10f37a6f58936494
Zero Creates <droneskyler35@gmail.com>
2026-09-24 15:46:29 -0500

    Update Post 1.5.4

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ from PIL import Image, ImageDraw
 from flask_cors import CORS
 from dateutil import parser
 
-CURRENT_VERSION = "1.5.3"
+CURRENT_VERSION = "1.5.4"
 
 VERSION_URL = "https://raw.githubusercontent.com/ZeroCreates/Chaster-API-To-Resonite/main/CurrentVersion.txt"
 
